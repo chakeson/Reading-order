955c9e71f6dd54ceec3667aea7b57c6914a4f791
chakeson <80765479+chakeson@users.noreply.github.com>
2022-01-20 21:52:26 +0100

    Implement pan zoom pinch

--- reading-order/src/index.tsx
+++ reading-order/src/index.tsx
@@ -4,11 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <TransformWrapper initialScale={1} initialPositionX={0} initialPositionY={0}>
+        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+            <React.Fragment>
+                <div className="tools">
+                    <button onClick={() => zoomIn()}>+</button>
+                    <button onClick={() => zoomOut()}>-</button>
+                    <button onClick={() => resetTransform()}>x</button>
+                </div>
+                <TransformComponent>
+                    <App />
+                </TransformComponent>
+             </React.Fragment>
+        )}
+        </TransformWrapper>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
