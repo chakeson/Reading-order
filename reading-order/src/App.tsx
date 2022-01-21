aba190743adb063edd387e21674bd12ce457edfd
chakeson <80765479+chakeson@users.noreply.github.com>
2022-01-21 03:18:39 +0100

    Added tailwind 3.

--- reading-order/src/App.tsx
+++ reading-order/src/App.tsx
@@ -4,14 +4,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-full">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link wheelDisabled"
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
