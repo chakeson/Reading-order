33a12fe466029e51979fe9bb5146b88dc0d4a4b4
chakeson <80765479+chakeson@users.noreply.github.com>
2022-03-02 21:14:21 +0100

    Updated zone compenent so borders can be specified and added zone data.

--- reading-order/src/components/zone.tsx
+++ reading-order/src/components/zone.tsx
@@ -10,15 +10,21 @@ interface Props {
     id:number;
     title:string;
     icon?:SVGAElement;
+    borderWidth?:string;
+    borderStyle?:string;
 }
 
+
 function Zone(props:Props) {
-    const { x , y , width, height , xtitle , ytitle , textwidth , id , title , icon } = props;
+    const { x , y , width, height , xtitle , ytitle , textwidth , id , title , icon , borderWidth , borderStyle } = props;
 
     var idAsString:string = id.toString()+"zone"
 
+    const borderWidthText = borderWidth || "8px";
+    const borderStyleText = borderStyle || "solid";
+
     return ( 
-        <div id={idAsString} style={{top:`${y}px`,left:`${x}px`,width:`${width}px`,height:`${height}px`}} className="border-2 absolute rounded-2xl">
+        <div id={idAsString} style={{top:`${y}px`,left:`${x}px`,width:`${width}px`,height:`${height}px`,borderWidth:`${borderWidthText}`, borderStyle:`${borderStyleText}`}} className="absolute rounded-2xl">
                 <h3 style={{top:`${ytitle}px`,left:`${xtitle}px`,width:`${textwidth}px`}} className="absolute font-semibold text-5xl inline-flex flex-col justify-center items-center">
                     {title}
                     {icon}
