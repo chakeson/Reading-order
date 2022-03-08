8f2a964768a454ef3a9e8214ff00c54f5abfc110
chakeson <80765479+chakeson@users.noreply.github.com>
2022-03-08 18:00:30 +0100

    Base of context provider with localstorage built.

--- reading-order/src/context.tsx
+++ reading-order/src/context.tsx
@@ -1,12 +1,33 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useState , useEffect} from "react";
 /*type ProviderType = {
     horusHeresy:[],
     siegeOfTerra:[]
 }*/
 const AppContext = React.createContext({});
 
+// Array filled with 0 length 299
+// Array.apply(null, Array(299)).map(function (x) { return 0; });
+
+const storageAccess = () => {
+    var lsReadingProgress:number[] = Array.apply(null, Array(299)).map(function (x) { return 0; });
+    if (localStorage.getItem("ReadingProgress")){
+        let lsTemp:string = localStorage.getItem("ReadingProgress") || "";
+        lsReadingProgress = JSON.parse(lsTemp)
+    }
+    return lsReadingProgress;
+}
+
+
+
+
 const AppProvider: React.FC = ({ children }) => {
-    const [readingProgress, setReadingProgress] = useState<any>(null);
+    const [readingProgress, setReadingProgress] = useState<number[]>(storageAccess());
+
+    useEffect(() => {
+        localStorage.setItem('ReadingProgress', JSON.stringify(readingProgress));
+    },[readingProgress])
+
+
 
     return (
         <AppContext.Provider value={{ readingProgress , setReadingProgress }}>
