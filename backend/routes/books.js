993cbe253e115f54f7ee388054f315139bd56bb3
chakeson <80765479+chakeson@users.noreply.github.com>
2022-03-30 23:14:48 +0200

    Books API endpoint

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,39 +1,67 @@
 const express = require("express")
-const router = express.Router()
 const book = require("../models/book")
 
 // CRUD CREATE READ UPDATE DELETE
+//      POST   PUT  PUT    None
+
+// Create books
+exports.postBooks = async function(req, res) {
+    // Verify input
+    var unverifiedUserKey = req.body.userkey;
+    var unverifiedBook = req.body.book;
+    
+    // TODO - Verify input
+    var verifiedUserKey = unverifiedUserKey;
+    var verifiedBook = unverifiedBook;
+
+    var book = new book({
+        userkey: unverifiedUserKey,
+        book: unverifiedBook
+    });
+
+    book.save(function(err) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } 
+        else {
+            res.send("Success.");
+        }
+    });
+};
+
+// Update book
+exports.putBooks = async function(req, res) {
+    // Verify input
+    var unverifiedUserKey = req.body.userkey;
+    var unverifiedBook = req.body.book;
+    
+    // TODO - Verify input
+    var verifiedBook = unverifiedBook;
+
+    book.findByIdAndUpdate({ userkey: userKey }, { book: verifiedBook }, function(err, books) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } 
+        else {
+            res.send("Success.");
+        }
+    });
+};
 
 
 // Read user data
-router.get('/', async (req, res)=> {
-    try {
-        
-        res.send("Hello World")
-    } catch (error) {
-        
-    }
-})
+exports.getBooks = async function(req, res) {
+    var userKey = req.body.userkey;
 
-
-router.get('/test/:id', (req, res)=> {
-    res.send(req.params.id)
-})
-
-// Creating user
-router.post("/:create", (req, res)=> {
-    
-})
-
-// Updating user book
-router.patch("/:id", (req, res)=> {
-
-})
-
-// Delete user info
-router.delete("/:id", (req, res)=> {
-
-})
-
-
-module.exports = router
+    book.findById({ userkey: userKey }, function(err, books) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } 
+        else {
+            res.send(books);
+        }
+    });
+};
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ var express = require("express");
 var bodyParser = require("body-parser"); 
 var { default: mongoose } = require("mongoose");
 
+var bookRouter = require("./routes/books");
 
 var port = process.env.PORT || 3000;
 
@@ -19,10 +20,19 @@ const db = mongoose.connection
 db.on("error", (error)=>console.error(error))
 db.once("open", ()=>console.log("Connected to database."))
 
+
+// Create our Express router
+const router = express.Router();
+
+// Books routes
+router.route("/books").post(bookRouter.postBooks).get(bookRouter.getBooks).put(bookRouter.putBooks);
+
+/*
 // Create our Express router
 const bookRouter = require("./routes/books")
 
 app.use("/books", bookRouter)
+*/
 
 // Starts the express server
 app.listen(port, () => console.log("Server started."));
