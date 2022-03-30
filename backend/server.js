679ab691e26de78df188d81bd01aad9c5710d023
chakeson <80765479+chakeson@users.noreply.github.com>
2022-03-30 21:54:47 +0200

    Added bodyparser to application and wrote more of server core.

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
-require("dotenv").config()
+// Imports of libraries
+require("dotenv").config();
+var express = require("express");
+var bodyParser = require("body-parser"); 
+var { default: mongoose } = require("mongoose");
 
-const express = require("express");
-const { default: mongoose } = require("mongoose");
+
+var port = process.env.PORT || 3000;
+
+// Start/Create express server
 const app = express();
 
+// Use body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Database connection.
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on("error", (error)=>console.error(error))
 db.once("open", ()=>console.log("Connected to database."))
 
-
-app.use(express.json())
-
+// Create our Express router
 const bookRouter = require("./routes/books")
+
 app.use("/books", bookRouter)
 
-
-app.listen(3000, () => console.log("Server started."));
+// Starts the express server
+app.listen(port, () => console.log("Server started."));
