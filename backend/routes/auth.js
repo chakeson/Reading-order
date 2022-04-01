fda5aeb68339e2449ad62e96201e462a279faaa2
chakeson <80765479+chakeson@users.noreply.github.com>
2022-04-01 14:58:07 +0200

    Updated variable name.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,15 +6,15 @@ const User = require('../models/user');
 // Basic passport strategy HTTP
 passport.use(new BasicStrategy(
     function(username, password, callback) {
-        User.findOne({ username: username }, function (err, user) {
-            if (err) { return callback(err); }
+        User.findOne({ username: username }, function (error, user) {
+            if (error) { return callback(error); }
 
             // No user found with that username
             if (!user) { return callback(null, false); }
 
             // Make sure the password is correct
-            user.verifyPassword(password, function(err, isMatch) {
-                if (err) { return callback(err); }
+            user.verifyPassword(password, function(error, isMatch) {
+                if (error) { return callback(error); }
 
                 // Password did not match
                 if (!isMatch) { return callback(null, false); }
