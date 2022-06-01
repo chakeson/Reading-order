cc5b0144bdb6aa1b50eb4243d1f4b7c722529d95
chakeson <80765479+chakeson@users.noreply.github.com>
2022-06-01 14:40:41 +0200

    Sync component css.

--- reading-order/src/components/syncVisualiser.tsx
+++ reading-order/src/components/syncVisualiser.tsx
@@ -19,9 +19,11 @@ function SyncVisualiser() {
 
 
     return ( 
-        <div className={`${syncStatus.message==="" ? "opacity-0" : "opacity-90"} absolute flex justify-center items-center bottom-1/4 mx-auto right-0 left-0 w-24 border-solid border-1 rounded-2xl border-black z-30 font-medium`} style={{backgroundColor:syncStatus.color}}>
-            {syncStatus.message}
-        </div>
+        <span className={`${syncStatus.message==="" ? "opacity-0" : "opacity-90"} transform transition duration-300 absolute inline-flex justify-center items-center bottom-1/4 mx-auto right-0 left-0 max-w-40 min-w-24 z-30`}>
+            <span className="px-2 py-1 border-solid border-2 border-black rounded-md font-medium" style={{backgroundColor:syncStatus.color}}>
+                {syncStatus.message}
+            </span>
+        </span>
     );
 }
 
