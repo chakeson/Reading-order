9b86e12738f300c3bdb330e2a166e446c96f5589
chakeson <80765479+chakeson@users.noreply.github.com>
2022-06-16 16:40:08 +0200

    Sync div now completly hidden when not shown.

--- reading-order/src/components/syncVisualiser.tsx
+++ reading-order/src/components/syncVisualiser.tsx
@@ -16,8 +16,8 @@ function SyncVisualiser() {
     }, [syncStatus.message]);
 
     return ( 
-        <span className={`${syncStatus.message==="" ? "opacity-0" : "opacity-95"} transform transition duration-300 absolute inline-flex justify-center items-center bottom-1/4 mx-auto right-0 left-0 max-w-40 min-w-24 z-30`}>
-            <span className="px-2 py-1 border-solid border-2 border-black rounded-md font-medium" style={{backgroundColor:syncStatus.color}}>
+        <span className={`${syncStatus.message==="" ? "opacity-0 max-w-0 min-w-0" : "opacity-95 max-w-40 min-w-24"} transform transition duration-300 absolute inline-flex justify-center items-center bottom-1/4 mx-auto right-0 left-0 z-30`}>
+            <span className={`${syncStatus.message==="" ? "p-0 border-0" : "px-2 py-1 border-2"} border-solid border-black rounded-md font-medium`} style={{backgroundColor:syncStatus.color}}>
                 {syncStatus.message}
             </span>
         </span>
