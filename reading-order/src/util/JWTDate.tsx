e021a76d61a07513dfd17d23424c43a60ebf7039
chakeson <80765479+chakeson@users.noreply.github.com>
2022-07-07 17:21:06 +0200

    Commented and guarded the decode functions input.

--- reading-order/src/util/JWTDate.tsx
+++ reading-order/src/util/JWTDate.tsx
@@ -1,9 +1,15 @@
 import { authObject } from "../context";
 
+// Parses the payload of the JWT token and returns it as an object.
 export const jwtTokenDecode = (jwt:string) => {
+    if (jwt.length === 0 || jwt === undefined || jwt === null) {
+        return {};
+    }
     return JSON.parse(atob(jwt.split('.')[1]));
 }
 
+// Checks if the token has expired and returns true or false depending on it.
+// If there is no token returns undefined.
 export const jwtTokenIsExpired = (auth:authObject) => {
 
     // Check if there is an actual token.
